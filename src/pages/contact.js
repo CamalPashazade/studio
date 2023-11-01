b5f39dca4f9cd8cc896f300100f01cc93c0b5ded
CamalPashazade <camal24011992@gmail.com>
2023-11-01 15:56:28 +0400

    sfssfsf

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,30 @@
-export async function getServerSideProps() {
-  const res = await fetch("http://localhost:8080/getadmin.php/");
-  const repo = await res.json();
-  return { props: { repo } };
-}
+// export async function getServerSideProps() {
+//   const res = await fetch("http://localhost:8080/getadmin.php/");
+//   const repo = await res.json();
+//   return { props: { repo } };
+// }
 
-const Contact = ({ repo }) => {
-  console.log(typeof repo);
+import { useEffect, useState } from "react";
+
+const Contact = () => {
+  
+    const [data, setData] = useState(false)
+
+ 
+  useEffect(() => {
+    fetch('http://localhost:8080/getadmin.php/')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+    
+      })
+  }, [])
+
   return (
     <div className="flex w-full  flex-col items-center justify-center">
       <div className="px-3 container bg-pink-400 ">
         Contact us
-        {repo.map((item) => (
+        {data.map((item) => (
           <div key={item.id}>
             <h1> {item.name}</h1>
             <b>{item.email}</b>
