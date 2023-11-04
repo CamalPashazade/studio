bdab3b2168e9c55f565ead34722954da810ae697
CamalPashazade <camal24011992@gmail.com>
2023-11-04 16:02:33 +0400

    lang

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Mobile from "./mobile/Mobile";
+import Langdropdown from "../langdropdown/Langdropdown";
 
 const Header = () => {
   const containerStyle = {
@@ -25,7 +26,7 @@ const Header = () => {
         <source src="/video/video.mp4" type="video/mp4" />
       </video>
 
-      <div className="flex justify-between container items-center">
+      <div className="flex justify-between container items-center ">
         <Link href="/">
           <Image
             width={60}
@@ -36,7 +37,7 @@ const Header = () => {
           />
         </Link>
 
-        <div>
+        <div className="flex">
           <nav className="hidden  top-3 text-lg  items-center md:flex ">
             <Link className="pl-3 text-white" href="/">
               Home
@@ -50,6 +51,18 @@ const Header = () => {
           </nav>
 
           <Mobile />
+
+          <div className=" ml-2 hidden items-center  md:flex  ">
+            <div className="w-[30px]">
+              <Image
+                src="/langspinner/earth.gif"
+                width={5}
+                height={5}
+                layout="responsive"
+              />
+            </div>
+            <Langdropdown />
+          </div>
         </div>
       </div>
     </div>
